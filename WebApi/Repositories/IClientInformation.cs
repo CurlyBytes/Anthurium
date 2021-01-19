d31588eca69b5adaa7a3e9955beaf3daa8ce1db4
cocoytech <francisco.abayon@rafi.ph>
2021-01-19 20:48:38 +0800

    Feat(repository): add joborder and client info repository

---
+++ WebApi/Repositories/IClientInformation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Models;
+
+namespace WebApi.Repositories
+{
+    public interface IClientInformation
+    {
+        bool SaveChanges();
+
+        IEnumerable<ClientInformation> GetJobOrders();
+        ClientInformation GetJobOrderById(int Id);
+
+        void NewClientInformation(ClientInformation clientInformation);
+
+        void UpdateClientInformation(ClientInformation clientInformation);
+
+        void RemoveClientInformation(ClientInformation clientInformation);
+
+        ClientInformation NewClientByDateWithin30Days();
+
+        ClientInformation RunningTotalOfClients(int Id);
+    }
+}
---
+++ WebApi/Repositories/IJobOrderRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Models;
+
+namespace WebApi.Repositories
+{
+    public interface IJobOrderRepository
+    {
+        bool SaveChanges();
+
+        IEnumerable<JobOrder> GetJobOrders();
+        JobOrder GetJobOrderById(int Id);
+
+        void CreateJobOrder(JobOrder JobOrder);
+
+        void UpdateCJobOrder(JobOrder JobOrder);
+
+        void DeleteJobOrder(JobOrder JobOrder);
+
+        JobOrder RunningTotalOfCompletedJobOrder(int Id);
+    }
+}
