3eacda4d60ce12f41d227287edeecc28dffa3556
cocoytech <francisco.abayon@rafi.ph>
2021-01-19 21:08:31 +0800

    Feat(dbcontext): enable ef framework 6 via repository pattern

---
+++ WebApi/Data/AnthuriumContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Models;
+
+namespace WebApi.Data
+{
+    public class AnthuriumContext : DbContext
+    {
+        public AnthuriumContext(DbContextOptions<AnthuriumContext> opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<ClientInformation> ClientInformations { get; set; }
+        public DbSet<JobOrder> JobOrders { get; set; }
+        public DbSet<JobOrderDescriptionOfWork> JobOrderDescriptionOfWorks { get; set; }
+    }
+}
--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -1,21 +1,49 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json.Serialization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApi.Data;
+using WebApi.Repositories;
+using WebApi.Repositories.SqlServer;
 
 namespace WebApi
 {
     public class Startup
     {
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers().AddNewtonsoftJson(s => {
+                s.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            });
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Anthurium", Version = "v1" });
+            });
+
+            services.AddDbContext<AnthuriumContext>(opt => opt.UseSqlServer
+            (Configuration.GetConnectionString("CommanderConnection")));
+
+            services.AddScoped<IClientInformation, SqlServerClientInformationRepository>();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
