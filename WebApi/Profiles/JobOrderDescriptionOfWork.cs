ab62496eeaa76beaa568aeab47f03974248587f5
cocoytech <francisco.abayon@rafi.ph>
2021-01-20 02:26:06 +0800

    Feat(profile): create automapper profiles in joborder and jobrderdescriptionofwork

---
+++ WebApi/Profiles/JobOrderDescriptionOfWork.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Dtos;
+
+namespace WebApi.Profiles
+{
+    public class JobOrderDescriptionOfWork : Profile
+    {
+        public JobOrderDescriptionOfWork()
+        {
+            //source->target
+            CreateMap<JobOrderDescriptionOfWork, JobOrderDescriptionOfWorkReadDto>();
+            CreateMap<JobOrderDescriptionOfWorkCreateDto, JobOrderDescriptionOfWork>();
+            CreateMap<JobOrderDescriptionOfWorkUpdateDto, JobOrderDescriptionOfWork>();
+            CreateMap<JobOrderDescriptionOfWork, JobOrderDescriptionOfWorkUpdateDto>();
+        }
+    }
+}
---
+++ WebApi/Profiles/JobOrderProfile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Dtos;
+using WebApi.Models;
+using AutoMapper;
+
+namespace WebApi.Profiles
+{
+    public class JobOrderProfile : Profile
+    {
+        public JobOrderProfile()
+        {
+            //source->target
+            CreateMap<JobOrder, JobOrderReadDto>();
+            CreateMap<JobOrderReadDto, JobOrder>();
+            CreateMap<JobOrderUpdateDto, JobOrder>();
+            CreateMap<JobOrder, JobOrderUpdateDto>();
+        }
+    }
+}
