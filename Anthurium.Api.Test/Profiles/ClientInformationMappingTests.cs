3da2f8fca1957e335faa7014da82779d92c3de68
cocoytech <francisco.abayon@rafi.ph>
2021-02-10 01:56:26 +0800

    Test(module): client information automapper profile

--- Anthurium.Api.Test/Profiles/ClientInformationMappingTests.cs
+++ Anthurium.Api.Test/Profiles/ClientInformationMappingTests.cs
@@ -56,7 +56,69 @@ namespace Anthurium.Api.Test.Profiles
             Assert.IsType<ClientInformationReadDto>(dto);
         }
 
-    
+        [Fact]
+        public void Should_AutoMapWorking_ClientInformationCreateDtoToClientInformation()
+        {
+            var dto = new ClientInformationCreateDto();
+            var entity = _mapper.Map<ClientInformation>(dto);
+
+            Assert.NotNull(entity);
+            Assert.IsType<ClientInformation>(entity);
+        }
+
+        [Fact]
+        public void Should_AutoMapWorking_ClientInformationUpdateDtoToClientInformationo()
+        {
+            var dto = new ClientInformationUpdateDto();
+            var entity = _mapper.Map<ClientInformation>(dto);
+
+            Assert.NotNull(entity);
+            Assert.IsType<ClientInformation>(entity);
+        }
+
+        [Fact]
+        public void Should_AutoMapWorking_ClientInformationToClientInformationUpdateDto()
+        {
+            var entity = new ClientInformation();
+            var dto = _mapper.Map<ClientInformationUpdateDto>(entity);
+
+            Assert.NotNull(dto);
+            Assert.IsType<ClientInformationUpdateDto>(dto);
+        }
+
+
+        [Fact]
+        public void Should_AutoMapWorking_ClientInformationReadDtoToClientInformationUpdateDto()
+        {
+            var sourceDto = new ClientInformationReadDto();
+            var targetDto = _mapper.Map<ClientInformationUpdateDto>(sourceDto);
+
+            Assert.NotNull(targetDto);
+            Assert.IsType<ClientInformationUpdateDto>(targetDto);
+        }
+
+
+        [Fact]
+        public void Should_AutoMapWorking_ClientInformationReadDtoToJobOrderChangeCreate()
+        {
+            var sourceDto = new ClientInformationReadDto();
+            var targetDto = _mapper.Map<JobOrderChangeCreate>(sourceDto);
+
+            Assert.NotNull(targetDto);
+            Assert.IsType<JobOrderChangeCreate>(targetDto);
+        }
+
+        [Fact]
+        public void Should_AutoMapWorking_JobOrderChangeCreateToJobOrderUpdateDto()
+        {
+            var sourceDto = new JobOrderChangeCreate();
+            var targetDto = _mapper.Map<JobOrderUpdateDto>(sourceDto);
+
+            Assert.NotNull(targetDto);
+            Assert.IsType<JobOrderUpdateDto>(targetDto);
+        }
+
+        
 
     }
 }
