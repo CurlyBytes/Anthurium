9d22d3117c93c6326cab77dbdc2122a612403d04
cocoytech <francisco.abayon@rafi.ph>
2021-02-11 16:02:21 +0800

    Chore(contract): web client for api service contracts

---
+++ Anthurium.Web/Contracts/DashboardApiResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Anthurium.Web.Contracts
+{
+    public class DashboardApiResponse
+    {
+        [JsonPropertyName("@odata.count")]
+        public int Count { get; set; }
+
+       // [JsonPropertyName("value")]
+       
+    }
+}
